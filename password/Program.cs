8ef3302333d34e12c9122a492a236715aaf6fdd2
leatherbullet <al.obnizov@yandex.ru>
2024-02-28 09:37:07 +0300

    Добавьте файлы проекта.

---
+++ password/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace password
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "12345";
+            int userTryInput = 3;
+            string userInput;
+
+            while (userTryInput-- > 0)
+            {
+                Console.WriteLine($"введите пароль: ");
+                userInput = Console.ReadLine();
+                
+                if (password == userInput)
+                {
+                    Console.WriteLine("тайное сообщение");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"пароль не верный. у вас осталось {userTryInput} попытки");
+                }
+            }
+        }
+    }
+}
