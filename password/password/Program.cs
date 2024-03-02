1432be14f5367c65c7c50e202d8181e7d5ee8677
leatherbullet <143252652+leatherbullet@users.noreply.github.com>
2024-03-02 11:29:23 +0300

    Add files via upload

---
+++ password/password/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace password
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "12345";
+            int userTryInput = 3;
+            string userInput;
+
+            for (int i = userTryInput; i > 0; i--)
+            {
+                Console.WriteLine("для доступа введите пароль.\n количество попыток:" + i);
+                userInput = Console.ReadLine();
+
+                if (userInput == password)
+                {
+                    Console.WriteLine("секретное сообщение");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("попробуйте еще раз");
+                }
+            } 
+        }
+    }
+}
